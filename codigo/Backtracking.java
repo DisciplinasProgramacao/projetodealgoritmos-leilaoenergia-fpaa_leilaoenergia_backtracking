9e61ce55b3739e26f9e5a1468c8a65f576703f9d
Pedro Henrique Moreira Caixeta Ferreira <phmcf13@gmail.com>
2024-06-11 22:03:04 -0300

    implementacao classe backtracking

--- codigo/Backtracking.java
+++ codigo/Backtracking.java
@@ -1,7 +1,26 @@
+import java.util.List;
+
 public class Backtracking {
-    public static void main(String[] args) {
-        System.out.println("Olá, Backtracking");
+
+    public int resolver(List<Lance> lances, int energiaTotal) {
+        return backtracking(lances, energiaTotal, 0, 0);
+    }
+
+    private int backtracking(List<Lance> lances, int energiaRestante, int valorAtual, int melhorValor) {
+        if (energiaRestante == 0) {
+            return Math.max(melhorValor, valorAtual);
+        }
+
+        for (Lance lance : lances) {
+            if (lance.getEnergia() <= energiaRestante) {
+                int valorPossivel = valorAtual + lance.getValor();
+                if (valorPossivel > melhorValor) {
+                    melhorValor = backtracking(lances, energiaRestante - lance.getEnergia(),
+                            valorAtual + lance.getValor(), melhorValor);
+                }
+            }
+        }
+
+        return melhorValor;
     }
 }
-
-
