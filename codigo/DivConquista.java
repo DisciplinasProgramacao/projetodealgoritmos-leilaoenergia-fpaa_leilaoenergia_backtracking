febdf7ef19810dc2b782a4c3327eab4d8b2dfd33
Gabriel Alejandro Figueiro Galindo <83712515+gabriel6988@users.noreply.github.com>
2024-06-18 21:34:27 -0300

    Update DivConquista.java

--- codigo/DivConquista.java
+++ codigo/DivConquista.java
@@ -1,8 +1,25 @@
+import java.util.List;
 public class DivConquista {
-    public static void main(String[] args) {
-        System.out.println("Olá, Divisão e Conquista");
+
+    public int resolver(List<Lance> lances, int energiaTotal) {
+        return divisaoEConquista(lances, energiaTotal);
+    }
+
+    private int divisaoEConquista(List<Lance> lances, int energiaRestante) {
+        if (energiaRestante == 0 || lances.isEmpty()) {
+            return 0;
+        }
+
+        Lance lanceAtual = lances.get(0);
+        List<Lance> lancesRestantes = lances.subList(1, lances.size());
+
+        int incluirLance = 0;
+        if (lanceAtual.getEnergia() <= energiaRestante) {
+            incluirLance = lanceAtual.getValor() + divisaoEConquista(lancesRestantes, energiaRestante - lanceAtual.getEnergia());
+        }
+
+        int excluirLance = divisaoEConquista(lancesRestantes, energiaRestante);
+
+        return Math.max(incluirLance, excluirLance);
     }
 }
-
-
-
