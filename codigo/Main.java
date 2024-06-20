fef82f1e712c4270b6256b0c6bbf28898717535d
lucasht02 <lucashemetrio@gmail.com>
2024-06-20 17:38:08 -0300

    conjunto

--- codigo/Main.java
+++ codigo/Main.java
@@ -6,14 +6,33 @@ public class Main {
     public static void main(String[] args) {
         List<Lance> lances = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        lances.add(new Lance(500, 500));
-        lances.add(new Lance(500, 510));
-        lances.add(new Lance(400, 520));
-        lances.add(new Lance(300, 400));
-        lances.add(new Lance(200, 220));
-        lances.add(new Lance(900, 1110));
+        lances.add(new Lance(430, 1043));
+        lances.add(new Lance(428, 1188));
+        lances.add(new Lance(410, 1565));
+        lances.add(new Lance(385, 1333));
+        lances.add(new Lance(399, 1214));
+        lances.add(new Lance(382, 1498));
+        lances.add(new Lance(416, 1540));
+        lances.add(new Lance(436, 1172));
+        lances.add(new Lance(416, 1386));
+        lances.add(new Lance(423, 1097));
+        lances.add(new Lance(400, 1463));
+        lances.add(new Lance(406, 1353));
+        lances.add(new Lance(403, 1568));
+        lances.add(new Lance(390, 1228));
+        lances.add(new Lance(387, 1542));
+        lances.add(new Lance(390, 1206));
+        lances.add(new Lance(430, 1175));
+        lances.add(new Lance(397, 1492));
+        lances.add(new Lance(392, 1293));
+        lances.add(new Lance(393, 1533));
+        lances.add(new Lance(439, 1149));
+        lances.add(new Lance(403, 1277));
+        lances.add(new Lance(415, 1624));
+        lances.add(new Lance(387, 1280));
+        lances.add(new Lance(417, 1330));
 
-        int energiaTotal = 1000;
+        int energiaTotal = 8000;
 
         System.out.println("Escolha uma opção:");
         System.out.println("1. Backtracking");
@@ -36,21 +55,21 @@ public class Main {
                 break;
             case 2:
                 // Testando algoritmo guloso 1 (maior valor)
-                Guloso algoritmoGuloso1 = new Guloso();
-                long inicioGuloso1 = System.currentTimeMillis();
-                int melhorValorGuloso1 = algoritmoGuloso1.resolverEstrategia1(lances, energiaTotal);
-                long fimGuloso1 = System.currentTimeMillis();
-                System.out.println("Melhor valor obtido (Guloso 1): " + melhorValorGuloso1);
-                System.out.println("Tempo de execução (Guloso 1): " + (fimGuloso1 - inicioGuloso1) + "ms");
+//                Guloso algoritmoGuloso1 = new Guloso();
+//                long inicioGuloso1 = System.currentTimeMillis();
+//                int melhorValorGuloso1 = algoritmoGuloso1.resolverEstrategia1(lances, energiaTotal);
+//                long fimGuloso1 = System.currentTimeMillis();
+//                System.out.println("Melhor valor obtido (Guloso 1): " + melhorValorGuloso1);
+//                System.out.println("Tempo de execução (Guloso 1): " + (fimGuloso1 - inicioGuloso1) + "ms");
                 break;
             case 3:
                 // Testando algoritmo guloso 2 (melhor razão valor/energia)
-                Guloso algoritmoGuloso2 = new Guloso();
-                long inicioGuloso2 = System.currentTimeMillis();
-                int melhorValorGuloso2 = algoritmoGuloso2.resolverEstrategia2(lances, energiaTotal);
-                long fimGuloso2 = System.currentTimeMillis();
-                System.out.println("Melhor valor obtido (Guloso 2): " + melhorValorGuloso2);
-                System.out.println("Tempo de execução (Guloso 2): " + (fimGuloso2 - inicioGuloso2) + "ms");
+//                Guloso algoritmoGuloso2 = new Guloso();
+//                long inicioGuloso2 = System.currentTimeMillis();
+//                int melhorValorGuloso2 = algoritmoGuloso2.resolverEstrategia2(lances, energiaTotal);
+//                long fimGuloso2 = System.currentTimeMillis();
+//                System.out.println("Melhor valor obtido (Guloso 2): " + melhorValorGuloso2);
+//                System.out.println("Tempo de execução (Guloso 2): " + (fimGuloso2 - inicioGuloso2) + "ms");
                 break;
             case 4:
                 // Testando divisão e conquista
